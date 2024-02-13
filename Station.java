1eecdaeeccdd0b1435665948fba65455bfbff479
Evan Chen <echen26@students.polytechnic.org>
2024-02-13 12:26:03 -0800

    fields, constructors, get ID and listpass method

--- Station.java
+++ Station.java
@@ -1,3 +1,31 @@
+import java.util.*;
 public class Station {
+
+    /* fields */
+    private ArrayList<Passenger> pass;
+    private String ID;
+    private static int idgen;
+
+    /* constructors */
+    public Station(){
+        pass = new ArrayList<Passenger>();
+
+        ID = "S" + idgen;
+        idgen++;        
+    }
+
+
+    /* methods */
+    public String getID(){
+        return ID;
+    }
+
+    public String listPass(){
+        String overall = "Passengers at Station " + ID + ":";
+        for(int i = 0; i < pass.size(); i++){
+            overall += "\n" + pass.get(i).toString();
+        }
+        return overall;
+    }
     
 }
