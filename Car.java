c97eef2625358c067fe58f0fd462d5624bbc5bfe
Evan Chen <echen26@students.polytechnic.org>
2024-02-13 11:57:48 -0800

    Fields, constructors, ID gen.

--- Car.java
+++ Car.java
@@ -1,3 +1,33 @@
+import java.util.*;
 public class Car {
+    /* fields */
+    private ArrayList<Passenger> pass;
+    private int destination;
+    private int location;
+    private boolean dirUp;
+    private String ID;
+    private static int idgen;
     
+
+    /* constructors */
+    public Car(){
+
+        pass = new ArrayList<Passenger>();
+
+        int randomDestination = (int) (Math.random()*36);
+        destination = randomDestination;
+
+        int randomStart = (int) (Math.random()*36);
+        location = randomStart;
+
+        if(destination > location){
+            dirUp = true;
+        } else {
+            dirUp = false;
+        }
+
+        ID = "P" + idgen;
+        idgen++;
+
+    }
 }
