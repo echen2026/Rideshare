0ab37b61a865816d59b3be343c4a72e2a12ac31f
Evan Chen <echen26@students.polytechnic.org>
2024-02-20 14:29:34 -0800

    Began tester file

---
+++ RideshareTester.java
@@ -0,0 +1,15 @@
+public class RideshareTester {
+    
+
+
+    public static void main(String[] args) {
+        System.out.println("Rideshare Project");
+        System.out.println();
+
+
+        Road r = new Road(3, 6);
+        System.out.println(r);
+    }
+
+
+}
