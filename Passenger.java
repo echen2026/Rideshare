78de1726b92fcac35e8f92359f116dad36d4dd97
Evan Chen <echen26@students.polytechnic.org>
2024-02-25 16:13:25 -0800

    Simplified passenger

--- Passenger.java
+++ Passenger.java
@@ -1,63 +1,23 @@
 public class Passenger{
 
     /* fields */
-    private boolean dirUp;
     private int destination;
-    private int location;
-    private String ID;
+    private int ID;
     private static int idgen;
 
     /* constructors */
-    public Passenger(){
+    public Passenger(int mydestination){
 
-        int randomDestination = (int) (Math.random()*32);
-        destination = randomDestination;
+        destination = mydestination;
 
-        int randomStart = (int) (Math.random()*32);
-        location = randomStart;
-
-        if(destination > location){
-            dirUp = true;
-        } else {
-            dirUp = false;
-        }
-
-
-        ID = "P" + idgen;
+        ID = idgen;
         idgen++;
 
     }
-
-
+    
     /* methods */
-
     public int getDes(){
         return destination;
     }
-
-    public int getLoc(){
-        return location;
-    }
-
-    public boolean getDir(){
-        return dirUp;
-    }
-
-    public String getID(){
-        return ID;
-    }
-
-    public String toString(){
-        String directiontext = "";
-        if(dirUp == false){
-            directiontext = "Down";
-        } else {
-            directiontext = "Up";
-        }
-
-        return "Passenger ID: " + ID + " | Destination: " + destination + " | Direction: " + directiontext;
-    }
-
-
 
 }
